c1721fee004570545beb28fec41681741bc9c976
Gursimran Singh <g13singh@gmail.com>
2013-05-22 00:47:02 -0500

    "basic functions to query and store everything"

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
                  [clj-http "0.7.2"]
                  [io.curtis/boilerpipe-clj "0.2.0"]
                  [org.clojure/java.jdbc "0.3.0-alpha4"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]])
+                 [org.xerial/sqlite-jdbc "3.7.2"]
+                 [org.clojure/data.json "0.2.2"]])
--- src/newscat/core.clj
+++ src/newscat/core.clj
@@ -1,8 +1,11 @@
 (ns newscat.core
-  (:require [clojure.string :as str]
+  (:require [clojure.string :as string]
             [clojure.java.jdbc :as j]
-            [clojure.java.jdbc.sql :as s])
+            [clojure.java.jdbc.sql :as s]
+            [clj-http.client :as client]
+            [clojure.data.json :as json])
   (:use [boilerpipe-clj.core]))
+
 
 (defn get-article-text
   "Given a URL, extracts the article text and returns it as a string"
@@ -10,7 +13,7 @@
   (get-text (slurp url)))
 
 (def query-template
-  "http://www.reddit.com/r/subreddit/new.json?sort=new&limit=100")
+  "http://www.reddit.com/r/subreddit/new.json?sort=top&limit=50")
 
 (def categories
   ["politics" "business" "technology"
@@ -34,6 +37,40 @@
   []
   (map #(create-cat-table %) categories))
 
+(defn run-query
+  "Given a category, run a query and return results"
+  [category query-template]
+  (try (:body
+        (client/get
+         (string/replace query-template
+                         #"subreddit"
+                         category)))
+       (catch Exception e (println e))))
+
+
+(defn extract-text
+  "given the result of a query, extract the article text and return it
+   as a list of maps of the form {:url url, :content article-text}"
+  [query-result]
+  (map #(hash-map :url (->> % :data :url)
+                  :content (get-article-text (->> % :data :url)))
+       (->> (json/read-str query-result :key-fn keyword)
+            :data
+            :children)))
+
+
+(defn store-query
+  "given a category and a query template, stores result of query in table"
+  [category query-template]
+  (let [res (extract-text (run-query category query-template))]
+    (map #(j/insert! db category %)
+         res)))
+
+(defn query-and-store-all
+  "runs queries and stores results for all categories"
+  [categories query-template]
+  (map #(store-query % query-template)
+       categories))
 
 (defn foo
   "I don't do a whole lot."
